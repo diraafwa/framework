b9674cf3a07b0000b5f069856e82b950d2c860bd
diraafwa <diramardanela@gmail.com>
2022-09-29 13:15:46 +0700

    Connect API

---
+++ WebApplication4MCC/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication4MCC.ViewModels;
+
+namespace WebApplication4MCC.Controllers
+{
+    HttpClient 
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(Login login)
+        {
+
+        }
+    }
+}
